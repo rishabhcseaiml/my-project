3999ebb4e12cb0592dff6bf0c1c9df37d3c35f40
rishabh raikwar <145255409+rishabhcseaiml@users.noreply.github.com>
2025-09-12 15:44:58 +0530

    commit

---
+++ React-project/Experiment-9/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PersonHierarchy from './components/PersonClassHierarchy.jsx';
+
+export default function App() {
+  return (
+    <div>
+      <PersonHierarchy />
+    </div>
+  );
+}
